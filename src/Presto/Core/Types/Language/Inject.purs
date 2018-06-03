a34ccf7012bc152077fa40b1de2ef40b345a92c8
Eric Wong <ericsyw@gmail.com>
2018-06-03 10:42:06 +0800

    Add the Inject class and instances

---
+++ src/Presto/Core/Types/Language/Inject.purs
@@ -0,0 +1,23 @@
+module Presto.Core.Types.Language.Inject where
+
+import Prelude
+
+import Data.Either (Either(..))
+import Data.Functor.Coproduct (Coproduct(..), coproduct)
+import Data.Maybe (Maybe(..))
+
+class Inject f g where
+    inj :: forall a. f a -> g a
+    prj :: forall a. g a -> Maybe (f a)
+
+instance injectIdentity :: Inject f f where
+    inj = id
+    prj = Just
+
+instance injectLeftCoproduct :: Inject f (Coproduct f g) where
+    inj = Coproduct <<< Left
+    prj = coproduct Just (const Nothing)
+
+instance injectRightCoproduct :: Inject f g => Inject f (Coproduct f g) where
+    inj = Coproduct <<< Right <<< inj
+    prj = coproduct (const Nothing) prj
