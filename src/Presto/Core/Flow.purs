8840d7b18b61294c9f2b3ecf84ff1e7f1a19babe
Eric Wong <ericsyw@gmail.com>
2018-06-08 13:38:25 +0800

    Export specific functions and data types

--- src/Presto/Core/Flow.purs
+++ src/Presto/Core/Flow.purs
@@ -1,10 +1,19 @@
 module Presto.Core.Flow ( module Presto.Core.Language.Runtime.Flow
                         , module Presto.Core.Types.Language.Flow
+                        , module Presto.Core.Types.Language.API
+                        , module Presto.Core.Types.Language.Store
+                        , module Presto.Core.Types.Language.UI
+                        , module Presto.Core.Types.Language.Permission
                         , module Presto.Core.Types.Language.Interaction
                         , module Presto.Core.Types.Language.Storage
                         ) where
 
-import Presto.Core.Types.Language.Flow
-import Presto.Core.Language.Runtime.Flow
+import Presto.Core.Language.Runtime.Flow (runFlow)
+import Presto.Core.Types.Language.API (APIResult, ApiF, callAPI)
+import Presto.Core.Types.Language.Flow (PermissionCheckRunner, PermissionRunner(..), PermissionTakeRunner, Runtime(..), Control, Flow, fork, launch, doAff, await, await', delay, oneOf)
 import Presto.Core.Types.Language.Interaction (class Interact, Interaction, defaultInteract, request)
+import Presto.Core.Types.Language.Permission (PermissionF, checkPermissions, takePermissions)
 import Presto.Core.Types.Language.Storage (class Serializable, Key)
+import Presto.Core.Types.Language.Store (Store(..), StoreF, get, set, load, save)
+import Presto.Core.Types.Language.UI (GuiF, UIResult, evalUI, forkUI, initUI, initUIWithScreen, runUI, runUI', showUI, runScreen, forkScreen, throwErr, withError)
+
