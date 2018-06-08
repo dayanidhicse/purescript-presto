3099bc800e569294e21709b9ce8b6347c2efb7a1
Eric Wong <ericsyw@gmail.com>
2018-06-08 14:07:23 +0800

    Export UIRunner and APIRunner type

--- src/Presto/Core/Flow.purs
+++ src/Presto/Core/Flow.purs
@@ -1,4 +1,5 @@
 module Presto.Core.Flow ( module Presto.Core.Language.Runtime.Flow
+                        , module Presto.Core.Language.Runtime.Interaction
                         , module Presto.Core.Types.Language.Flow
                         , module Presto.Core.Types.Language.API
                         , module Presto.Core.Types.Language.Store
@@ -9,6 +10,7 @@ module Presto.Core.Flow ( module Presto.Core.Language.Runtime.Flow
                         ) where
 
 import Presto.Core.Language.Runtime.Flow (runFlow)
+import Presto.Core.Language.Runtime.Interaction (APIRunner, UIRunner)
 import Presto.Core.Types.Language.API (APIResult, ApiF, callAPI)
 import Presto.Core.Types.Language.Flow (PermissionCheckRunner, PermissionRunner(..), PermissionTakeRunner, Runtime(..), Control, Flow, fork, launch, doAff, await, await', delay, oneOf)
 import Presto.Core.Types.Language.Interaction (class Interact, Interaction, defaultInteract, request)
@@ -16,4 +18,3 @@ import Presto.Core.Types.Language.Permission (PermissionF, checkPermissions, tak
 import Presto.Core.Types.Language.Storage (class Serializable, Key)
 import Presto.Core.Types.Language.Store (Store(..), StoreF, get, set, load, save)
 import Presto.Core.Types.Language.UI (GuiF, UIResult, evalUI, forkUI, initUI, initUIWithScreen, runUI, runUI', showUI, runScreen, forkScreen, throwErr, withError)
-
