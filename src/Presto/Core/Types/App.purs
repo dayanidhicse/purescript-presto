d99971beeb1fcc766c924ae526932e2afce44a6c
Eric Wong <ericsyw@gmail.com>
2018-07-01 09:42:27 +0800

    Add new Effects in AppEffects

--- src/Presto/Core/Types/App.purs
+++ src/Presto/Core/Types/App.purs
@@ -12,16 +12,17 @@ import Control.Monad.Aff (Aff)
 import Control.Monad.Aff.AVar (AVAR)
 import Control.Monad.Aff.Console (CONSOLE)
 import Control.Monad.Eff (kind Effect)
-import Control.Monad.Eff.Ref (REF)
 import Control.Monad.Eff.Exception (EXCEPTION)
+import Control.Monad.Eff.Ref (REF)
 import DOM (DOM)
 import FRP (FRP)
+import WebSocket (WEBSOCKET)
 
 foreign import data LOCAL_STORAGE :: Effect
 foreign import data STORAGE :: Effect
 foreign import data NETWORK :: Effect
 foreign import data UI :: Effect
 
-type AppEffects eff = (avar :: AVAR, frp :: FRP, dom :: DOM, ref :: REF, ui :: UI, storage :: STORAGE, ls :: LOCAL_STORAGE, exception :: EXCEPTION, network :: NETWORK, console :: CONSOLE | eff)
+type AppEffects eff = (avar :: AVAR, frp :: FRP, dom :: DOM, ref :: REF, ui :: UI, storage :: STORAGE, ls :: LOCAL_STORAGE, exception :: EXCEPTION, network :: NETWORK, console :: CONSOLE, err :: EXCEPTION, ws :: WEBSOCKET | eff)
 type AppFlow eff = Aff (AppEffects eff)
 type UIFlow eff = Aff (frp :: FRP, dom :: DOM | eff)
